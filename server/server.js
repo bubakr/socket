5d35b010b3369d57936f44548b363f403b724f80
Ahmad Abubakr <ahmad@tqniat.com>
2017-07-31 15:54:41 +0200

    Emit new message across browsers

--- server/server.js
+++ server/server.js
@@ -13,9 +13,33 @@ var io = socketIO(server);
 io.on('connection', (socket)=>{
     console.log('new user connected');
 
+    socket.emit('newEmail', {
+        from: 'email@domain.com',
+        text: 'Hey, what is going on?',
+        created: 123
+    });
+
+    socket.on('createMessage', (message)=>{
+        console.log('Message created', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            created: new Date().getTime()
+        });
+    });
+
+
     socket.on('disconnect', ()=>{
         console.log('Disconncted');
     });
+
+
+    socket.on('createEmail', (data)=>{
+        console.log('Create Email', data);
+    });
+
+
+
 });
 
 app.use(express.static(publicPath));
